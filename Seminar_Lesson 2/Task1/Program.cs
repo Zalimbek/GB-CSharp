72da862a1d6d499346509f63084be574995e8e8c
Zhamilia Alimbekova <jamilysoul@mail.ru>
2024-01-14 18:46:21 +0300

    Seminar2: Task1 & 2

---
+++ Seminar_Lesson 2/Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿// void numToQuad(int num)
+// {
+//    System.Console.WriteLine($"Quad is: {num * num}");
+// }
+// System.Console.WriteLine("Input number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// numToQuad(number);
+
+// Not void method
+int numToQuad2(int num)
+{
+   return num * num;
+}
+System.Console.WriteLine("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(numToQuad2(number));
---
+++ Seminar_Lesson 2/Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿// на вход трехзначное число, удаляем вторую цифру
+using System.ComponentModel;
+
+void deteleSecondDigit(int num)
+{
+    if(num > 99 && num <= 999)
+    { 
+        System.Console.WriteLine($"{num} -> {num/100 * 10 + num % 10}");
+    }
+    else
+    {
+        System.Console.WriteLine("The input not 3 digit number!");
+    }
+}
+System.Console.WriteLine("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+deteleSecondDigit(number);
