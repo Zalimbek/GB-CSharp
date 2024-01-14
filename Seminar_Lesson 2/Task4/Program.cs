bd1de260e4d7c2344d44451dd7aeff740a67ab28
Zhamilia Alimbekova <jamilysoul@mail.ru>
2024-01-14 19:38:58 +0300

    Seminar2: Task4

---
+++ Seminar_Lesson 2/Task4/Program.cs
@@ -0,0 +1,19 @@
+﻿// на вход 2 числа и выводить , является ли второе число кратным первому. Если число не кратно первому,
+// то программа выврдит остаток от деления
+
+void isDelete(int num1, int num2)
+{
+    if (num1 % num2 == 0)
+    {
+        System.Console.WriteLine($"The second number {num2} is a multiple of {num1}");
+    }
+    else
+    {
+        System.Console.WriteLine($"The second number {num2} is not a multiple of {num1}. Remainder is {num1 % num2}");
+    }
+}
+System.Console.WriteLine("Input first number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input second number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+isDelete(number1, number2);
