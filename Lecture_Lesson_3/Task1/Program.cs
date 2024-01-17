d0093706cb35cedb1ad1dfd1b3200095f47f4447
Zhamilia Alimbekova <jamilysoul@mail.ru>
2024-01-17 14:45:38 +0300

    Lecture3: Task1

---
+++ Lecture_Lesson_3/Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿double CalculateFormula(int a, int b, int c, int d)
+{
+    double numenator = a * b;
+    int denominator = c + d;
+    double result = numenator / denominator;
+    return result;
+}
+
+double result = CalculateFormula(1, 2, 3, 4);
+System.Console.WriteLine(result);
