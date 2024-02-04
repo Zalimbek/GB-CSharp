6df3838f6e15ce67d6eeb0f47f33263ba3b04f90
Zhamilia Alimbekova <jamilysoul@mail.ru>
2024-02-04 20:02:11 +0300

    Added HW for seminar 6

---
+++ Seminar_Lesson_6/Homework/Program.cs
@@ -0,0 +1,47 @@
+﻿// // Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+
+// string CharsToString(char[,] chars)
+// {
+//     string str = "";
+//     for (int i = 0; i < chars.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < chars.GetLength(1); j++)
+//         {
+//             str += chars[i,j]; 
+//         }
+//     } return str;
+
+// }
+
+// char[,] inputStr = {{'q','w','e'},{'a','s','d'}};
+// System.Console.WriteLine(CharsToString(inputStr));
+
+// Задайте строку, содержащую латинские буквы в обоих регистрах. 
+//Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+
+// System.Console.WriteLine("Type any word: ");
+// string input = Console.ReadLine().ToLower();
+// Console.WriteLine(input);
+
+// Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+ bool Palindrom(string str)
+
+ {
+     string letters= new string(str.Where(char.IsLetterOrDigit).ToArray()); 
+     //string letters_rev = string.letters.Reverse();
+     System.Console.WriteLine(letters.Reverse());
+        for(int i = 0; i < letters.Length; i++)
+            if(letters[i] != letters[letters.Length - i - 1]) 
+            {
+                return false;
+            } return true;
+        
+            
+        }
+ 
+
+System.Console.WriteLine("Type any word: ");
+string input = Console.ReadLine().ToLower();
+System.Console.WriteLine(Palindrom(input));
---
+++ Seminar_Lesson_6/Program.cs
@@ -0,0 +1,65 @@
+﻿// // Создать строку из массива символов
+
+// string CharsToString (char[] chars)
+// {
+//     string str = "";
+//     for (int i = 0; i < chars.Length; i++)
+//     {
+//         str+= chars[i];
+//     } return str;
+// }
+
+// char[] chars = new char[]{'a', 'b', 'c', 'd'};
+// System.Console.WriteLine(CharsToString(chars));
+
+// //2 Method
+
+
+// string str = new string(chars);
+// System.Console.WriteLine(str);
+
+// Метод, который из строки делает массив символов
+
+// char[] StringToChars(string str)
+// {
+//     char[] chars = new char[str.Length];
+//     for (int i=0; i < str.Length; i++)
+//     {
+//         chars[i] = str[i];
+//     } return chars;
+// }
+
+// void PrintChars(char[] chars)
+// {
+//     for (int i = 0; i < chars.Length; i++)
+//     {
+//         System.Console.Write($"{chars[i]} ");
+//     }
+// }
+// string str = "Hello";
+// PrintChars(StringToChars(str));
+// //System.Console.WriteLine(StringToChars(str));
+
+// Считать строку с консоли латинских букв, метод должен вычислять кол-во гласных.
+
+
+int VowelsCnt(string str)
+{
+    int cnt = 0;
+    char[] arr = {'a', 'e','i', 'o','u','y' };
+     for (int i=0; i < str.Length; i++)
+     {
+       foreach (char e in arr)
+       {
+        if ( str[i]==e)
+        {
+            cnt++;
+        }
+       }  
+     } return cnt;
+
+}
+
+System.Console.WriteLine("Type any word: ");
+string input = Console.ReadLine().ToLower();
+Console.WriteLine(VowelsCnt(input));
