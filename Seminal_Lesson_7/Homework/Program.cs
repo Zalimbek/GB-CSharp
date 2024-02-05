74aa6abe62f54a24a2bd903e355dd0f165e06780
Zhamilia Alimbekova <jamilysoul@mail.ru>
2024-02-06 00:33:29 +0300

    Seminar 7: Added HW

---
+++ Seminal_Lesson_7/Homework/Program.cs
@@ -0,0 +1,55 @@
+﻿// void ValuesInRange(int m, int n) // 2 - 8
+// {
+//     if (n > 0 && n >= m)
+//     {
+//         ValuesInRange(m, n-1);
+//         System.Console.WriteLine(n + " ");
+//     }
+
+// }
+
+// System.Console.Write("Enter start valye M: ");
+// int M = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Enter end valye N: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// ValuesInRange(M, N);
+
+// using System.Runtime.InteropServices;
+
+// int Akkerman(int m, int n)
+//     {
+//         if (m < 0 || n < 0) 
+//         {
+//             System.Console.WriteLine("M and N must be positive!");
+//         }
+//         if (m == 0)
+//         {
+//             return n + 1;
+//         } 
+        
+//         if (n == 0) 
+//         {
+//             return Akkerman(m - 1, 1);
+//         } else
+//         {
+//             return Akkerman(m - 1, Akkerman(m, n - 1));
+//         }
+//     }
+// System.Console.Write("Enter start valye M: ");
+// int M = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Enter end valye N: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(Akkerman(M, N));
+
+
+void ArrayViceVersa(int[] arr, int i)
+{
+    if (i == arr.Length)
+    return;
+
+    ArrayViceVersa(arr, i+1);
+    System.Console.Write(arr[i] + " ");
+}
+int[] array = {1, 2, 5,0, 10,34};
+ArrayViceVersa(array, 0);
+
---
+++ Seminal_Lesson_7/Program.cs
@@ -0,0 +1,49 @@
+﻿// на вход подается число, получаем сумму его цифр - рекурсия
+
+// using System.Runtime.InteropServices;
+
+// int FindSum(int num)
+// {
+//     if (num > 0)
+//     {
+//         return num % 10 + FindSum(num / 10);
+//     }
+//     return 0;
+
+// }
+
+// Console.WriteLine(FindSum(456));
+
+// Задайте значение N. Напишите программу, которая выводит все натуральные числа в промежутке от 1 до N
+
+// void OneToN(int n)
+// {
+//     if (n > 0)
+//     {
+//         OneToN(n-1);
+//         System.Console.WriteLine(n + " ");
+//     }
+    
+// }
+
+// System.Console.Write("Ennter any valye N: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// OneToN(N);
+
+// Считать строку с консоли, содержащую латинские буквы. Вывести на экран латинские буквы этой строки
+
+
+void Sogl(string str, int i)
+{
+    if (i == str.Length)
+    return;
+    if (str[i] >= 'a' && str[i] <= 'z' && "aeiou".IndexOf(str[i])== -1)
+    {
+        System.Console.Write(str[i]);
+    }
+    Sogl(str, i+1);
+}
+
+
+string str = Console.ReadLine();
+Sogl(str, 0);
